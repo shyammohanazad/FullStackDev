bda72d4d13e15d9eb688499ad087bceb6e905e90
shyammohanazad <shyamthesisid@gmail.com>
2023-04-12 20:10:39 +0530

    fancy Login form created

--- README2.md
+++ README2.md
@@ -1,3 +1,3 @@
 # FULL STACK DEVELOPMENT #
 
-This repository is to learn Full Stack Development in s/w and web. and also repo of frequently used functional Modules to perform different functionalities.
+This repository is to learn Full Stack Development in s/w and web. and also repo of frequently used functional Modules to perform different functionalities/task.
---
+++ login2.js
@@ -0,0 +1,26 @@
+var rootprop=document.documentElement.style;
+var mode=true;
+
+function changeMode(){
+    if (mode){
+        darkMode();
+    } else {
+        lightMode();
+    }
+    mode=!mode
+}
+
+function lightMode(){
+    rootprop.setProperty("--background","rgba(230,230,230)");
+    rootprop.setProperty("--shadow1","rgba(119,119,119,0.5)");
+    rootprop.setProperty("--shadow2","rgba(255,255,255,0.85)");
+    rootprop.setProperty("--labelColor","black");
+}
+
+function darkMode(){
+    rootprop.setProperty("--background","rgba(9 25 33)");
+    rootprop.setProperty("--shadow1","rgba(0 0 0 / 45%)");
+    rootprop.setProperty("--shadow2","rgba(255 255 255 / 12%)");
+    rootprop.setProperty("--labelColor","rgba(255 255 255 / 59%)");
+
+}
